a4df3576ef5a20ed02a31f93360d5b5cae4377d6
Ales Bacul <alesbacul@gmail.com>
2024-03-26 07:30:46 +0100

    home page design

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from "react";
 import instance from "../../config/axios";
 import { MyLink } from "../components/MyLink";
 import { PokemonProps } from "@/types";
-
+import Link from "next/link";
 export default function Page() {
+  const [loading, setLoading] = useState(true);
   const [pokemons, setPokemons] = useState<PokemonProps[] | null>();
 
   // empty argument - only on first render
@@ -17,6 +18,7 @@ export default function Page() {
       .get("pokemon")
       .then((response) => {
         setPokemons(response.data.content);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
@@ -28,42 +30,75 @@ export default function Page() {
     instance
       .delete(`pokemon/${pokemonId}/delete`)
       .then((response) => {
-        alert("Pokémon byl vymazán úspěšně.");
-        setPokemons(pokemons?.filter((e) => e.id !== pokemonId));
+        if (response.status == 200)
+          setPokemons(pokemons?.filter((e) => e.id !== pokemonId));
       })
       .catch((error) => console.log(error));
   };
 
+  function Button({
+    href,
+    name,
+    onClick,
+    colorClass,
+  }: {
+    href?: string;
+    name: string;
+    onClick?: () => void;
+    colorClass?: string;
+  }) {
+    const additionalClass = colorClass ? colorClass : " bg-blue-500 hover:bg-blue-700 ";
+    const className =
+      additionalClass +
+      "text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline";
+    if (href) {
+      return (
+        <Link className={className} href={href}>
+          {name}
+        </Link>
+      );
+    } else if (onClick) {
+      return (
+        <button className={className} onClick={onClick}>
+          {name}
+        </button>
+      );
+    }
+  }
+
   function PokemonRow({
     pokemon,
-    className,
+    tdClassName,
+    trClassName,
   }: {
     pokemon: PokemonProps;
-    className: string;
+    tdClassName: string;
+    trClassName: string;
   }) {
     return (
-      <tr>
-        <td className={className}>{pokemon.name}</td>
-        <td className={className}>{pokemon.type}</td>
-        <td className={className + " flex"}>
-          <div className="flex gap-2">
-            <MyLink href={`/pokemon/${pokemon.id}`} name={"Detail"} />
-            <MyLink href={`/pokemon/${pokemon.id}/edit`} name={"Upravit"} />
-            <button onClick={() => handlePokemonDelete(pokemon.id)}>
-              Vymazat
-            </button>
-          </div>
+      <tr className={trClassName}>
+        <td className={tdClassName}>{pokemon.name}</td>
+        <td className={tdClassName}>{pokemon.type}</td>
+        <td className={tdClassName + " flex gap-2 justify-center"}>
+          <Button href={`/pokemon/${pokemon.id}`} name={"Detail"} />
+          <Button href={`/pokemon/${pokemon.id}/edit`} name={"Upravit"} />
+          <Button
+            onClick={() => handlePokemonDelete(pokemon.id)}
+            name="Vymazat"
+            colorClass="bg-red-500 hover:bg-red-700 "
+          />
         </td>
       </tr>
     );
   }
 
   function PokemonTable({ pokemons }: { pokemons: PokemonProps[] }) {
-    const cellClassName = "p-2 border border border-slate-400";
+    const cellClassName = "p-3";
+    const trClassName = "p-3 border-b-2 border-sky-100 hover:bg-sky-100";
     return (
-      <table className=" border-collapse table-auto border border-slate-400">
+      <table className="w-full table-auto ">
         <thead>
-          <tr>
+          <tr className={"p-3 border-b-2 border-sky-100"}>
             <th className={cellClassName}>Jméno</th>
             <th className={cellClassName}>Druh</th>
             <th className={cellClassName}>Operace</th>
@@ -71,12 +106,21 @@ export default function Page() {
         </thead>
         <tbody>
           {pokemons.map((e) => (
-            <PokemonRow className={cellClassName} key={e.id} pokemon={e} />
+            <PokemonRow
+              tdClassName={cellClassName}
+              trClassName={trClassName}
+              key={e.id}
+              pokemon={e}
+            />
           ))}
         </tbody>
       </table>
     );
   }
 
-  return <>{pokemons && <PokemonTable pokemons={pokemons} />}</>;
+  if (loading) {
+    return <>Načítání...</>;
+  } else {
+    return <>{pokemons && <PokemonTable pokemons={pokemons} />}</>;
+  }
 }
