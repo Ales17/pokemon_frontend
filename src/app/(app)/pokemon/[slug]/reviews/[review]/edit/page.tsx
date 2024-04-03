e0dc5fd0326c7987a6d23309ac9e801cc6ed1d31
Ales Bacul <alesbacul@gmail.com>
2024-04-03 16:55:51 +0200

    review edit page

---
+++ src/app/(app)/pokemon/[slug]/reviews/[review]/edit/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { useParams } from "next/navigation";
+import instance from "@/config/axios";
+import { useEffect, useState } from "react";
+import ReviewForm from "@/app/components/ReviewForm";
+import { ReviewProps } from "@/types";
+export default function Page() {
+  const { review, slug } = useParams();
+  const [reviewToUpdate, setReviewToUpdate] = useState<ReviewProps>();
+  useEffect(() => {
+    instance
+      .get(`pokemon/${slug}/reviews/${review}`, {})
+      .then(function (response) {
+        if (response.status != 200) {
+          alert("Chyba při načítání recenze");
+        } else {
+          console.log(response.data);
+          setReviewToUpdate(response.data);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      Upravit recenzi
+      <ReviewForm pokemonId={slug} reviewToUpdate={reviewToUpdate} />
+    </>
+  );
+}
