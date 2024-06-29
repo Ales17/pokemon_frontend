35cd00a2891e8f1a5f07f796ebfca89be3b6c9fb
Ales Bacul <alesbacul@gmail.com>
2024-06-29 11:20:07 +0200

    login page bs layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
 "use server";
 import "@/app/globals.css";
+import { Container, Row, Col } from "react-bootstrap";
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="cs" className="h-full bg-white">
-      <body className="h-full">{children}</body>
+    <html lang="cs">
+      <body><Container className="pt-3"><Row className="justify-content-center"><Col sm={6} md={4}>{children}</Col></Row></Container></body>
     </html>
   );
 }
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import instance from "@/config/axios";
 import { FormEvent, useState } from "react";
 import { setCookie } from "cookies-next";
 import { useRouter } from "next/navigation";
+import { Button, Form } from "react-bootstrap";
 export default function Page() {
   const [inputs, setInputs] = useState({ username: "", password: "" });
   const router = useRouter();
@@ -27,77 +28,49 @@ export default function Page() {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Přihlášení
-        </h2>
+    <div>
+      <div>
+        <h2>Přihlášení</h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form
+      <div>
+        <Form
           className="space-y-6"
           onSubmit={(e: FormEvent) => {
             handleLogin(e);
           }}
         >
-          <div>
-            <label
-              htmlFor="username"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Uživatelské jméno
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="username"
-                required
-                className="block w-full border-0 border-b border-blue-500 w-full py-2 px-3 text-gray-700"
-                onChange={(e: any) =>
-                  setInputs({ ...inputs, username: e.target.value })
-                }
-                value={inputs.username}
-              />
-            </div>
-          </div>
+          <Form.Group className="mb-3">
+            <Form.Label>Uživatelské jméno</Form.Label>
+            <Form.Control
+              type="text"
+              id="username"
+              name="username"
+              autoComplete="username"
+              onChange={(e: any) =>
+                setInputs({ ...inputs, username: e.target.value })
+              }
+              value={inputs.username}
+            ></Form.Control>
+          </Form.Group>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Heslo
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full border-0 border-b border-blue-500 w-full py-2 px-3 text-gray-700"
-                onChange={(e: any) =>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Heslo</Form.Label>
+            <Form.Control type="password" name="password" onChange={(e: any) =>
                   setInputs({ ...inputs, password: e.target.value })
                 }
-                value={inputs.password}
-              />
-            </div>
-          </div>
+                value={inputs.password}  required></Form.Control>
+          </Form.Group>
+
+          
 
           <div>
-            <button
-              type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Přihlásit se{" "}
-            </button>
+            <Button variant="primary" type="submit">
+              Přihlásit se
+            </Button>
           </div>
-        </form>
+        </Form>
       </div>
     </div>
   );
