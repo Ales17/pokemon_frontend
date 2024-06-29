aba7cfd41adb772927e2736a2016bc784367af82
Ales Bacul <alesbacul@gmail.com>
2024-06-29 17:43:15 +0200

    bs app layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,23 +1,17 @@
 "use client";
 import "@/app/globals.css";
-import { Navigation } from "../components/Navigation";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { UserPanel } from "../components/UserPanel";
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const navItems = [
-    { href: "/", name: "Domů" },
-    { href: "/pokemon/create", name: "Nový záznam" },
-  ];
-
   return (
     <html lang="cs">
-      <body className="flex flex-col md:px-[10vw] bg-sky-100">
-        <Navbar bg="dark" data-bs-theme="dark">
+      <body>
+        <Navbar bg="dark" data-bs-theme="dark" className="mb-3">
           <Container>
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
+            <Nav className="me-auto"> 
               <Nav.Link href="/">Domů</Nav.Link>
               <Nav.Link href="/pokemon/create">Nový záznam</Nav.Link>
             </Nav>
